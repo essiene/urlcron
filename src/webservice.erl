77e503dadf68cc753e096f8e79b291c2008248cb
Essien Ita Essien <essien.ita@uknglobal.com>
2008-11-27 18:33:09 +0100

    Skeleton hooked up all and done

--- src/webservice.erl
+++ src/webservice.erl
@@ -5,7 +5,9 @@
 
 
 get("/", Req) ->
+    get("", Req);
+get("", Req) ->
     QueryString = Req:parse_qs(),
-    Req:ok({"text/plain", io_lib:format("~s", [QueryString])});
+    Req:ok({"text/plain", io_lib:format("~p", [QueryString])});
 get(_Path, Req) ->
     Req:not_found().
