a636464372b38b14e110e58c2052cca44ec967b0
Akintayo A. Olusegun <segun.akintayo@uknglobal.com>
2008-11-28 16:38:23 +0100

    Basic Setup for new schedule.

--- src/urlcron_timecalc.erl
+++ src/urlcron_timecalc.erl
@@ -10,6 +10,8 @@ date_diff(Date) ->
     Now = calendar:now_to_local_time(erlang:now()),
     date_diff(Date, Now).
 
+
+
 date_diff(DateB, DateA) ->
     Secs = fun(A) -> calendar:datetime_to_gregorian_seconds(A) end,
     DateASecs = Secs(DateA),
--- src/webservice.erl
+++ src/webservice.erl
@@ -3,11 +3,39 @@
        get/2
     ]).
 
+get_value(Key, TupleList) ->
+    case lists:keysearch(Key, 1, TupleList) of
+        {value, {Key, Value}} ->
+            Value;
+        false ->
+            ""
+    end.            
+
+todate(QueryString) ->
+    YY = erlang:list_to_integer(get_value("YY", QueryString)),
+    MM = erlang:list_to_integer(get_value("MM", QueryString)),
+    DD = erlang:list_to_integer(get_value("DD", QueryString)),
+    Hh = erlang:list_to_integer(get_value("hh", QueryString)),
+    Mm = erlang:list_to_integer(get_value("mm", QueryString)),
+    Ss = erlang:list_to_integer(get_value("ss", QueryString)),
+    {{YY, MM, DD}, {Hh, Mm, Ss}}.
+
+get("/new", Req) ->
+    QueryString = Req:parse_qs(),
+    StartTime = todate(QueryString),    
+    Url = get_value("url", QueryString),
+    Name = get_value("url", QueryString),
+    io:fwrite("~p~n", [StartTime]),
+    io:fwrite("~p~n", [urlcron_timecalc:date_diff(StartTime)]),
+    RetVal = urlcron_scheduler:new(StartTime, Url),
+    Req:ok({"text/plain", io_lib:format("~p", [RetVal])});
 
 get("/", Req) ->
     get("", Req);
+
 get("", Req) ->
     QueryString = Req:parse_qs(),
     Req:ok({"text/plain", io_lib:format("~p", [QueryString])});
+
 get(_Path, Req) ->
     Req:not_found().
