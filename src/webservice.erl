e0abe9a7fa44bc1c7adce1188e994432ade4304c
Akintayo A. Olusegun <segun.akintayo@uknglobal.com>
2008-11-28 21:59:36 +0100

    Added the other url's to the webservice, no functionality yet just stubs
    Modified the parameters passed to new?

--- src/webservice.erl
+++ src/webservice.erl
@@ -8,32 +8,66 @@ get_value(Key, TupleList) ->
         {value, {Key, Value}} ->
             Value;
         false ->
-            ""
+            []
     end.            
 
 todate(QueryString) ->
-    YY = erlang:list_to_integer(get_value("YY", QueryString)),
-    MM = erlang:list_to_integer(get_value("MM", QueryString)),
-    DD = erlang:list_to_integer(get_value("DD", QueryString)),
-    Hh = erlang:list_to_integer(get_value("hh", QueryString)),
-    Mm = erlang:list_to_integer(get_value("mm", QueryString)),
-    Ss = erlang:list_to_integer(get_value("ss", QueryString)),
+    YY = erlang:list_to_integer(get_value("year", QueryString)),
+    MM = erlang:list_to_integer(get_value("month", QueryString)),
+    DD = erlang:list_to_integer(get_value("day", QueryString)),
+    Hh = erlang:list_to_integer(get_value("hour", QueryString)),
+    Mm = erlang:list_to_integer(get_value("minute", QueryString)),
+    Ss = erlang:list_to_integer(get_value("seconds", QueryString)),
     {{YY, MM, DD}, {Hh, Mm, Ss}}.
 
 get("/new", Req) ->
     QueryString = Req:parse_qs(),
-    StartTime = todate(QueryString),    
-    Url = get_value("url", QueryString),
-    Name = get_value("name", QueryString),
+    {StartTime, Url, Name} = get_basic_params(QueryString),
     RetVal = urlcron_scheduler:new(Name, StartTime, Url),
     Req:ok({"text/plain", io_lib:format("~p", [RetVal])});
+
+get("/update" ++ Name, Req) ->
+    QueryString = Req:parse_qs(),
+    {_StartTime, _Url, _Name} = get_basic_params(QueryString),    
+    %call update function
+    Req:ok({"text/plain", io_lib:format("~p:~p", [Name, QueryString])});
+
+get("/delete/all", Req) ->
+    %call delete_all function
+    Req:ok({"text/plain", io_lib:format("~p", [all])});
+
+get("/delete" ++ Name, Req) ->
+    %call delete function
+    Req:ok({"text/plain", io_lib:format("~p", [Name])});
+
+get("/view/all/", Req) ->
+    QueryString = Req:parse_qs(),
+    Format = get_value("format", QueryString),
+    %call view all function
+    Req:ok({"text/plain", io_lib:format("~p:~p", [all, Format])});
+
+get("/view/" ++ Name, Req) ->
+    QueryString = Req:parse_qs(),
+    Format = get_value("format", QueryString),
+    %call view function
+    Req:ok({"text/plain", io_lib:format("~p:~p", [Name, Format])});
+
+get("/stats", Req) ->
+    %statistical data
+    Req:ok({"text/plain", io_lib:format("~p", [stats])});
 
 get("/", Req) ->
     get("", Req);
 
 get("", Req) ->
-    QueryString = Req:parse_qs(),
-    Req:ok({"text/plain", io_lib:format("~p", [QueryString])});
+    Req:ok({"text/plain", io_lib:format("Welcome to ~p", [urlcron])});
 
 get(_Path, Req) ->
     Req:not_found().
+
+get_basic_params(QueryString) ->
+    StartTime = todate(QueryString),    
+    Url = get_value("url", QueryString),
+    Name = get_value("name", QueryString),
+    {StartTime, Url, Name}.
+
